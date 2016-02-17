66f7c7c4025d2887506d35e9ab36fd7cfd20451b
Siddharth Joshi <siddharth.joshi21@hotmail.com>
2016-02-17 23:19:12 +0530

    initial

---
+++ WebApplication9/Models/AutomationTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationWebTest.Models
+{
+    public class AutomationTask
+    {
+        public int Id { get; set; }
+    }
+}
