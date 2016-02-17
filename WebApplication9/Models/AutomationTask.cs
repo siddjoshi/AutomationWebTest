583af22555df5b1a2aa85b2342e547f423a2cc02
Siddharth Joshi <siddharth.joshi21@hotmail.com>
2016-02-17 23:22:40 +0530

    test

--- WebApplication9/Models/AutomationTask.cs
+++ WebApplication9/Models/AutomationTask.cs
@@ -9,5 +9,9 @@ namespace AutomationWebTest.Models
     public class AutomationTask
     {
         public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
     }
 }
